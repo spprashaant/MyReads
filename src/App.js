fd72e3cc684fcfd4330fe2703a84d88f6eed868f
Prashanth <spprashaant@gmail.com>
2018-08-11 18:14:09 +0530

    Fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
-
+import * as BooksAPI from './BooksAPI'
 class BooksApp extends React.Component {
   state = {
     /**
@@ -10,9 +10,15 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    books: []
   }
 
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books});
+    })
+  }
   render() {
     return (
       <div className="app">
